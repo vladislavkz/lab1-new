5009f738a5d8beef5bdee854a978ab2d1ebff6a9
vladislavkz <kazachenkovv@tksu.ru>
2025-11-27 01:36:54 +0300

    Initial commit

---
+++ src/components/Counter.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+export default function Counter() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="card">
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+      <p>
+        Counter component is working!
+      </p>
+    </div>
+  )
+}
